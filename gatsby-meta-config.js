66fe6d1763f556bdda5c7242a8b162097443226a
soulcactus <sunhyejin90@naver.com>
2019-09-01 17:05:03 +0900

    :running: Initial commit

---
+++ gatsby-meta-config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  title: `SOULCACTUS`,
+  description: `선인장의 개발 블로그`,
+  author: `Soulcactus`,
+  introduction: `Fron-End Developer & UX/UI designer`,
+  siteUrl: `https://soulcactus.netlify.com`,
+  social: {
+    twitter: ``,
+    github: `soulcactus`,
+    medium: ``,
+    facebook: ``,
+  },
+  icon: `content/assets/cactus.png`,
+  keywords: [
+    `블로그`,
+    `코딩`,
+    `개발`,
+    `프로그래밍`,
+    `프론트엔드`,
+    `프론트엔드 개발`,
+    `웹`,
+    `웹 개발`,
+    `자바스크립트`,
+    `자바스크립트 개발`,
+    `리액트`,
+    `리액트 개발`,
+  ],
+  comment: {
+    disqusShortName: '',
+    utterances: 'soulcactus/gatsby-starter-bee',
+  },
+  configs: {
+    countOfInitialPost: 10,
+  },
+  sponsor: {
+    buyMeACoffeeId: '',
+  },
+  share: {
+    facebookAppId: '',
+  },
+  ga: 'UA-145048791-1',
+}
---
+++ src/components/theme-switch/index.jsx
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react'
+import Switch from 'react-switch'
+
+import * as Dom from '../../utils/dom'
+import { THEME } from '../../constants'
+
+import './index.scss'
+
+function getTheme(checked) {
+  return checked ? THEME.DARK : THEME.LIGHT
+}
+
+function toggleTheme(theme) {
+  switch (theme) {
+    case THEME.LIGHT: {
+      Dom.addClassToBody(THEME.LIGHT)
+      Dom.removeClassToBody(THEME.DARK)
+      break
+    }
+    case THEME.DARK: {
+      Dom.addClassToBody(THEME.DARK)
+      Dom.removeClassToBody(THEME.LIGHT)
+      break
+    }
+  }
+}
+
+export const ThemeSwitch = () => {
+  const [checked, setChecked] = useState(false)
+
+  const handleChange = checked => {
+    const theme = getTheme(checked)
+
+    setChecked(checked)
+    toggleTheme(theme)
+  }
+
+  useEffect(() => {
+    const checked = Dom.hasClassOfBody(THEME.DARK)
+
+    handleChange(checked)
+  }, [])
+
+  return null
+}
